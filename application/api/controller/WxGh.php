e0c0630b76aa17278e2e3b724357af34998c2340
jeff <waagik@gmail.com>
2019-05-05 19:10:37 +0800

    被动消息回复

--- application/api/controller/WxGh.php
+++ application/api/controller/WxGh.php
@@ -2,30 +2,95 @@
 
 namespace app\api\controller;
 
+use app\api\service\EventMessageHandler;
+use EasyWeChat\Kernel\Messages\Message;
+use EasyWeChat\Kernel\Messages\Text;
+use EasyWeChat\Kernel\Messages\Image;
+use EasyWeChat\Kernel\Messages\Video;
+use EasyWeChat\Kernel\Messages\Voice;
+use think\App;
 use think\Controller;
 
 class WxGh extends Controller
 {
+    public static $gh = null;
+    public static $server = null;
+    public static $user = null;
+    
+    public function __construct(App $app = null)
+    {
+        parent::__construct($app);
+        
+        //    先初始化微信
+        self::$gh = app('wechat.official_account');
+        
+        self::$server = self::$gh->server;
+        self::$user = self::$gh->user;
+    }
+    
     public function index()
     {
-        //    先初始化微信
-        $app = app('wechat.official_account');
-/*        $app->server->push(function ($message) {
-            return 'hello,world';
-        });
-        $app->server->serve()->send();*/
-    
-        $server = $app->server;
-        $user = $app->user;
-    
-        $server->push(function($message) use ($user) {
-            $fromUser = $user->get($message['FromUserName']);
-            return "Hi, {$fromUser->nickname}! 欢迎关注 Jeffio!";
-            
+        self::$server->push(function ($msg) {
+//            \Log::write($msg);
+//            dump($msg);
+
 //            return "Hi, 欢迎关注 Jeffio!";
+            switch ($msg['MsgType']) {
+//                case 'event':
+//                    return '收到事件消息';
+//                    break;
+                case 'text':
+                    return new Text($msg['Content']);
+                    break;
+                
+                case 'image':
+                    return new Image($msg['MediaId']);
+                    break;
+                
+                case 'voice':
+                    return new Voice($msg['MediaId']);
+                    break;
+                
+                case 'video':
+                    return new Video($msg['MediaId'], [
+                        'title' => '',
+                        'description' => '...',
+                    ]);
+                    break;
+                
+                case 'location':
+                    $location = <<<default
+纬度: $msg->Location_X
+经度: $msg->Location_Y
+位置信息: $msg->Label
+地图缩放大小: $msg->Scale
+default;
+                    return new Text($location);
+                    break;
+                
+                case 'link':
+                    $link = <<<default
+消息标题: $msg->Title
+消息描述: $msg->Description
+消息链接: $msg->Url
+default;
+                    return new Text($link);
+                    break;
+                
+                case 'file':
+                    return '文件消息已收到~';
+                
+                // ... 其它消息
+                default:
+                    return '消息已收到~';
+                    break;
+            }
         });
-    
-        $server->serve()->send();
+        
+        //        $server->push([self::class,'eventHandler'],Message::EVENT);
+        self::$server->push(EventMessageHandler::class, Message::EVENT);
+        
+        self::$server->serve()->send();
     }
     
 }
